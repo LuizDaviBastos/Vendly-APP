d400a8db90f305c516c16f7989e66dc0e6b9d280
Davi Bastos <davi@easyteam.rocks>
2023-05-30 14:57:42 -0300

    setup icon to show when app is closed

--- copy-icons.bat
+++ copy-icons.bat
@@ -1,3 +1,24 @@
 
 REM - copy icons
-xcopy /y ".\src\assets\icon\push_icon.png" ".\android\app\src\main\res\drawable"
+xcopy /y ".\src\assets\push_icon_sm.png" ".\android\app\src\main\res\drawable"
+xcopy /y ".\src\assets\push_icon_lg.png" ".\android\app\src\main\res\drawable"
+
+xcopy /y ".\src\assets\push_icon_sm.png" ".\android\app\src\main\res\mipmap-hdpi"
+xcopy /y ".\src\assets\push_icon_lg.png" ".\android\app\src\main\res\mipmap-hdpi"
+
+xcopy /y ".\src\assets\push_icon_sm.png" ".\android\app\src\main\res\mipmap-ldpi"
+xcopy /y ".\src\assets\push_icon_lg.png" ".\android\app\src\main\res\mipmap-ldpi"
+
+xcopy /y ".\src\assets\push_icon_sm.png" ".\android\app\src\main\res\mipmap-mdpi"
+xcopy /y ".\src\assets\push_icon_lg.png" ".\android\app\src\main\res\mipmap-mdpi"
+
+xcopy /y ".\src\assets\push_icon_sm.png" ".\android\app\src\main\res\mipmap-xhdpi"
+xcopy /y ".\src\assets\push_icon_lg.png" ".\android\app\src\main\res\mipmap-xhdpi"
+
+xcopy /y ".\src\assets\push_icon_sm.png" ".\android\app\src\main\res\mipmap-xxhdpi"
+xcopy /y ".\src\assets\push_icon_lg.png" ".\android\app\src\main\res\mipmap-xxhdpi"
+
+xcopy /y ".\src\assets\push_icon_sm.png" ".\android\app\src\main\res\mipmap-xxxhdpi"
+xcopy /y ".\src\assets\push_icon_lg.png" ".\android\app\src\main\res\mipmap-xxxhdpi"
+
+xcopy /y ".\src\assets\colors.xml" ".\android\app\src\main\res\values"
